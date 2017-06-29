7af3b8c36253ac4de4ca1f58729a0ce0ea42b43a
David Starr <dxstarr@godaddy.com>
2017-06-29 10:39:09 -0700

    made apple implement IFruit

--- Fruit/src/com/pluralsight/fruit/Apple.java
+++ Fruit/src/com/pluralsight/fruit/Apple.java
@@ -6,14 +6,13 @@ public class Apple implements IFruit {
     private static final String NAME = "Apple";
     private static final int ROLL_MULTIPLIER = 10;
 
-    @Override
+
     public String getName() {
         return NAME;
     }
 
-    @Override
     public int getRollMultiplier() {
-        return 10;
+        return ROLL_MULTIPLIER;
     }
 }
 
