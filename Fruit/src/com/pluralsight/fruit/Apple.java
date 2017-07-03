20a040808518041bc471f4eff3c1f348df7d4c71
David Starr <dxstarr@godaddy.com>
2017-07-03 13:32:36 -0700

    Made some debugging-found bug fixes

--- Fruit/src/com/pluralsight/fruit/Apple.java
+++ Fruit/src/com/pluralsight/fruit/Apple.java
@@ -7,11 +7,15 @@ public class Apple implements IFruit {
     private static final int ROLL_MULTIPLIER = 10;
 
 
+    @Override
     public String getName() {
+        System.out.println("NAME: " + NAME);
         return NAME;
     }
 
+    @Override
     public int getRollMultiplier() {
+        System.out.println("ROLL_MULTIPLIER: " + ROLL_MULTIPLIER);
         return ROLL_MULTIPLIER;
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Falling Apples
 
 This is sample code for an instructional course.
+
+1. This is a course for introducing IJ to new users.
+1. This is a course for introducing IJ to new users.
+1. This is a course for introducing IJ to new users.
+
+
+
+
+
