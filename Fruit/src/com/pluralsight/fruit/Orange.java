fef1ab471280d28e4fb30abc8757695c0e27bab6
David Starr <dxstarr@godaddy.com>
2017-07-04 06:13:49 -0700

    Created the orange.

--- Fruit/src/com/pluralsight/fruit/Orange.java
+++ Fruit/src/com/pluralsight/fruit/Orange.java
@@ -4,6 +4,7 @@ package com.pluralsight.fruit;
  * Created by dxstarr on 7/3/17.
  */
 public class Orange implements IFruit {
+
     @Override
     public String getName() {
         return "Orange";
